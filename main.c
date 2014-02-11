d638f0b1f65cf4c711c87031f79ffed2fd770a8d
cuki <mauricio@krebs.com.br>
2014-02-11 11:21:12 -0200

    ultimo commit

--- main.c
+++ main.c
@@ -17,20 +17,7 @@ long tempo = 35000;
 long tempoAux = 0;
 short ctrl = FALSE;
 short trigger = FALSE;
-
-#INT_EXT
-void isr_ext() {
-	clear_interrupt(INT_EXT);
-	tempo -= 1000;
-	if (!tempo)
-		tempo = 65000;
-}
-
 int main(void) {
-
-	clear_interrupt(INT_EXT);
-	enable_interrupts(INT_EXT);
-	enable_interrupts(GLOBAL);
 
 	setup_ccp1(CCP_PWM);
 	setup_timer_2(T2_DIV_BY_1, 250, 1);
@@ -41,14 +28,11 @@ int main(void) {
 		if (cont == 8)
 			cont = 1;
 		output_d(cont);
-		if (tempo != 20000) {
+		if (tempo > 4000) {
 			if (!input(PIN_B1) && ctrl) {
 				ctrl = FALSE;
-				if (tempo > 30000)
-					tempo -= 1000;
-				else
-					tempo -= 100;
-			} else if (!ctrl)
+				tempo -= 500;
+			} else if (input(PIN_B1))
 				ctrl = TRUE;
 		}
 		if (tempoAux != tempo) {
